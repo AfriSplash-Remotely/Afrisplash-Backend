07f6d9fd1faa01bcfd790500d8ca383af0189315
romzy234 <cyrilogoh@gmail.com>
2022-10-05 22:11:06 +0100

    Hello Message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({
+    status : "success",
+    success:true,
+    data:"Hello From AfriSplash Server"
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/v1', indexRouter);
 app.use('/api/v1/user', usersRouter);
 
 // catch 404 and forward to error handler
