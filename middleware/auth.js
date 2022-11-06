bdaef3d89987e3d3ac96262e3ba3a1645fd67bc1
romzy234 <cyrilogoh@gmail.com>
2022-11-06 11:59:06 +0100

    Auth Graud

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,8 +3,8 @@ const asyncHandler = require("./async");
 const ErrorResponse = require("../utils/errorResponse");
 const User = require("../model/user");
 
-// Protect routes
-exports.protect = asyncHandler(async (req, res, next) => {
+// Protect routes for Candidate 
+exports.C_protect = asyncHandler(async (req, res, next) => {
   let token;
 
   if (
@@ -15,9 +15,9 @@ exports.protect = asyncHandler(async (req, res, next) => {
     token = req.headers.authorization.split(" ")[1];
     // Set token from cookie
   }
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token;
-  // }
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
 
   // Make sure token exists
   if (!token) {
@@ -29,7 +29,51 @@ exports.protect = asyncHandler(async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.user_id);
 
-    next();
+    if(req.user.user_type == "candidate"){
+      next()
+    }else if (req.user.user_type == "recuiter"){
+      return next(new ErrorResponse("Not authorized to access this route", 401));
+    }else{
+      return next(new ErrorResponse("User Data Is Not Valid", 400));
+    }
+  } catch (err) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
+});
+
+// Protect routes for Recuiter 
+exports.R_protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    // Set token from Bearer token in header
+    token = req.headers.authorization.split(" ")[1];
+    // Set token from cookie
+  }
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.user_id);
+
+    if(req.user.user_type == "recuiter"){
+      next()
+    }else if (req.user.user_type == "candidate"){
+      return next(new ErrorResponse("Not authorized to access this route", 401));
+    }else{
+      return next(new ErrorResponse("User Data Is Not Valid", 400));
+    }
   } catch (err) {
     return next(new ErrorResponse("Not authorized to access this route", 401));
   }
