e2145fce273db73dfc282bd91464f741c1ea4f79
romzy234 <cyrilogoh@gmail.com>
2022-12-09 02:09:31 +0100

    Minor Update

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,43 @@ const asyncHandler = require("./async");
 const ErrorResponse = require("../utils/errorResponse");
 const User = require("../model/user");
 
+//TODO: Review This
+/**Route Graud For `All User` -- *MIDDLEWARE* */
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    // Set token from Bearer token in header
+    token = req.headers.authorization.split(" ")[1];
+    // Set token from cookie
+  }
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.user_id);
+    if(req.user){
+      next()
+    }else{
+      return next(new ErrorResponse("User Data Is Not Valid", 400));
+    }
+  } catch (err) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
+});
+
+
 /**Route Graud For `Candidate` -- *MIDDLEWARE* */
 exports.C_protect = asyncHandler(async (req, res, next) => {
   let token;
