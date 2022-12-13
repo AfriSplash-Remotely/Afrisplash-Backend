b83e33d3425557f0d5d737c57167fac5cabc9dc5
romzy234 <cyrilogoh@gmail.com>
2022-12-13 09:41:25 +0100

    minor fix

--- model/post.js
+++ model/post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
   _author: {
     type: mongoose.SchemaTypes.ObjectId,
-    ref: "post",
+    ref: "user",
     required: true
   },
 
