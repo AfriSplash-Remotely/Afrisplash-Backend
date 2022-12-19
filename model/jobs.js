c0f03886f32b61cfa551556e2506f9c920bc83b3
romzy234 <cyrilogoh@gmail.com>
2022-12-19 22:52:33 +0100

    Added Job Model

--- model/jobs.js
+++ model/jobs.js
@@ -1,61 +1,57 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema({
-    _company: {
-        type: mongoose.SchemaTypes.ObjectId,
-        required: false,
-        ref: "company",
-        default: null,
-    },
-    _author: {
-        type: mongoose.SchemaTypes.ObjectId,
-        required: false,
-        ref: "user",
-        default: null,
-    },
-    location: {
-        type: String,
-        required: true,
-        enum: ["Download", "Register", "Visit", "Apply"],
-        default: "Visit",
-    },
-    level: {
-        type: String,
-        required: true,
-        enum: ["Intermidate", "Beginner", "Senior", "Junior", "All"],
-        default: "All",
-    },
-    type: {
-        type: String,
-        require: true,
-        default:null
-    },
-    salary: {
-        type: Object,
-        required:true,
-        default:{
-            amount:null,
-            currency:null,
-            period:null
-        }
-    },
-    market : {
-        type: Array,
-        default: [],
-      },
-    about: {
-        type: String,
-        default:null
-    },
-    about: {
-        type: String,
-        default:null
-    },
-    verify: {
+  _company: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: false,
+    ref: 'company',
+    default: null
+  },
+  _author: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: false,
+    ref: 'user',
+    default: null
+  },
+  location: {
+    type: String,
+    required: true,
+    enum: ['Download', 'Register', 'Visit', 'Apply'],
+    default: 'Visit'
+  },
+  level: {
+    type: String,
+    required: true,
+    enum: ['Intermidate', 'Beginner', 'Senior', 'Junior', 'All'],
+    default: 'All'
+  },
+  type: {
+    type: String,
+    require: true,
+    default: null
+  },
+  salary: {
+    type: Object,
+    required: true,
+    default: {
+      amount: null,
+      currency: null,
+      period: null
+    }
+  },
+  market: {
+    type: Array,
+    default: []
+  },
+  about: {
+    type: String,
+    default: null
+  },
+  verify: {
     type: Boolean,
     required: true,
     default: false
-    },
+  },
   private: {
     type: Boolean,
     required: true,
@@ -68,8 +64,8 @@ const jobSchema = new mongoose.Schema({
   },
   expire: {
     type: Date,
-    default: null,
-  },
+    default: null
+  }
 });
 
-module.exports = mongoose.model("job", jobSchema)
+module.exports = mongoose.model('job', jobSchema);
