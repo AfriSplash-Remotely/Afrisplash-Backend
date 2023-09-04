0466b86f7a4be3fadcab107c351f2877c103ab94
Timothy-py <adeyeyetimothy33@gmail.com>
2023-09-05 00:21:01 +0100

    never mind :)

--- utils/swagger.js
+++ utils/swagger.js
@@ -26,7 +26,6 @@ const options = {
         description: 'Production server'
       },
       {
-        // url: `http://localhost:${PORT}/api/v1`,
         url: `http://localhost:5000/api/v1`,
         description: 'Development server'
       }
